67d4203953f6d071be91f0aaaf455f1faa32e9b4
Phil Mike Jones <philmikejones@gmail.com>
2021-03-07 10:36:47 +0000

    add exclusions

--- conf.py
+++ conf.py
@@ -36,7 +36,11 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    '_build', 'docs',
+    'content',
+    'venv', 'Thumbs.db', '.DS_Store', '.gitignore', '.gitmodules'
+]
 
 
 # -- Options for HTML output -------------------------------------------------
